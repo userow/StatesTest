0a46f7dd433226048adaa00711bf99946c8ddf61
Pavel Vasilenko (skype userow, telegram @userow81) <userow@gmail.com>
2019-06-18 17:09:39 +0300

    Initial Commit

---
+++ StatesTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StatesTest
+//
+//  Created by Paul Vasilenko on 6/18/19.
+//  Copyright © 2019 Paul Vasilenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
